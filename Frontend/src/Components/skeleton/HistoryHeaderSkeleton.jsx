12021244eb7bfc5656a1b7399e26f4b951edea98
jvlp <jvlp@cin.ufpe.br>
2022-10-14 11:42:41 -0300

    spinning animation on update button

--- Frontend/src/Components/skeleton/HistoryHeaderSkeleton.jsx
+++ Frontend/src/Components/skeleton/HistoryHeaderSkeleton.jsx
@@ -21,7 +21,25 @@ export default function HistoryHeaderSkeleton() {
           <span className='text-2xl sm:text-5xl mb-2'>
             <Skeleton width={300} />
           </span>
-          <button className='bg-slate-700 text-2xl rounded-lg'>update</button>
+          <button
+            type='button'
+            className='flex items-center rounded-lg bg-slate-700 px-4 py-2 text-2xl'
+            disabled
+          >
+            <svg
+              className='mr-3 h-5 w-5 animate-spin text-white'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+            >
+              <path
+                className='opacity-75'
+                fill='currentColor'
+                d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+              ></path>
+            </svg>
+            <span className='font-medium'> Updating... </span>
+          </button>
         </div>
       </div>
     </SkeletonTheme>
