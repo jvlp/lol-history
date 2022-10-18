e629f5370328900b4ddbbbcac31569078b14d184
jvlp <jvlp@cin.ufpe.br>
2022-10-18 19:55:56 -0300

    HistoryHeaderInfo component done

--- Frontend/src/Components/HistoryHeaderInfo.jsx
+++ Frontend/src/Components/HistoryHeaderInfo.jsx
@@ -3,28 +3,39 @@ import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 export default function HistoryHeaderInfo({ data }) {
+  const numMatches = data.length;
   const numWin = data.filter((match) => match.info.win).length;
-  const percentage = (numWin / data.length) * 100;
-  const numLoss = data.length - numWin;
+  const percentage = Math.floor((numWin / numMatches) * 100);
+  const numLoss = numMatches - numWin;
 
   const counts = new Array(999).fill(0);
   const countsWin = new Array(999).fill(0);
+  const comulativeKda = new Array(999).fill(0);
 
   data.map((match) => {
     const id = match.info.championId;
+    const { win, kills, deaths, assists } = match.info;
     counts[id]++;
-    countsWin[id] = match.info.win ? countsWin[id] + 1 : countsWin[id];
+    countsWin[id] = win ? countsWin[id] + 1 : countsWin[id];
+    comulativeKda[id] += (kills + assists) / deaths;
   });
 
-  const topPlayed = [...counts]
+  const countsCpy = [...counts];
+
+  const topPlayed = [...countsCpy]
     .sort((a, b) => b - a)
     .slice(0, 3)
-    .map((count) => counts.indexOf(count));
-
+    .map((count) => {
+      const index = countsCpy.indexOf(count);
+      countsCpy[index] = 0;
+      
+      return index;
+    });
+  console.log();
   return (
-    <div className='flex flex-row justify-center items-center'>
+    <div className='hidden lg:flex flex-row justify-center items-center'>
       <div className='flex flex-col items-center m-4'>
-        <span>Last 20 matches</span>
+        <span>{`Last ${numMatches} matches`}</span>
         <span>
           {numWin}W - {numLoss}L
         </span>
@@ -38,10 +49,9 @@ export default function HistoryHeaderInfo({ data }) {
       </div>
 
       {topPlayed.map((id, index) => (
-        <div className='flex flex-row mx-4'>
+        <div className='hidden xl:flex flex-row mx-4' key={index}>
           <img
             src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-tiles/${id}/${id}000.jpg`}
-            key={index}
             className={'w-16 h-16 mr-1'}
           ></img>
           <div className='flex flex-col'>
@@ -56,9 +66,10 @@ export default function HistoryHeaderInfo({ data }) {
             >
               {Math.floor((countsWin[id] / counts[id]) * 100)}%
             </span>
-            <span>
-              {countsWin[id]}V - {counts[id] - countsWin[id]}
+            <span className='text-gray-300'>
+              {countsWin[id]}V - {counts[id] - countsWin[id]}D
             </span>
+            <span className='text-gray-300'> {(comulativeKda[id] / counts[id]).toFixed(1) + ' '}KDA</span>
           </div>
         </div>
       ))}
